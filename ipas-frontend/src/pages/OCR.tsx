93e5d4635a322264d63e233cb1ccf97d44ead68a
MSSHARSHITH345826 <345826@nttdata.com>
2025-10-22 08:30:24 -0700

    harshith 22oct 2nd

--- ipas-frontend/src/pages/OCR.tsx
+++ ipas-frontend/src/pages/OCR.tsx
@@ -5,7 +5,6 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import $ from 'jquery';
 
 
 
